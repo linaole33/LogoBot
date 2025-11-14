5db45e353b882ee26c5c0e3c50e50210a11a1b55
linaole33 <lina.dorohina33@gmail.com>
2025-11-14 17:28:13 +0300

    Создание текстового бота

---
+++ bot.py
@@ -0,0 +1,13 @@
+import telebot;
+bot = telebot.TeleBot('8020115600:AAEuWDPnVxRkT2H_1HA7G_Ar7rZ1g7K0WvE');
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+bot.polling(none_stop=True, interval=0)

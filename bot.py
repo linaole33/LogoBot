0d3a93820bf8245c18e1e52eb29ae07b3b013ea0
linaole33 <lina.dorohina33@gmail.com>
2025-11-23 00:11:03 +0300

    Создала гитигнор в целях конфиденциальности

--- bot.py
+++ bot.py
@@ -1,13 +1,40 @@
 import telebot;
 bot = telebot.TeleBot('8020115600:AAEuWDPnVxRkT2H_1HA7G_Ar7rZ1g7K0WvE');
 
+#Считываю в массив user_list список пользователей из файла
+user_list = []
+users = open("users.txt", "a+", encoding="utf-8")  # a+ — открыть на чтение/дозапись, создать если нет
+users.seek(0)  # переходим в начало файла, чтобы прочитать содержимое
+
+for line in users:
+    user_list.append(line.strip())
+
+#Оставляем файл открытым на запись (указатель уже в конце)
+#Событие на получение сообщения
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
+    #Создаю временную переменную для сравнения (чтобы строка сравнивалась со строкой)
+    user_id_str = str(message.from_user.id)
+
+    #Условие, есть ли такой юзер в списке пользователей
+    if user_id_str in user_list:
+        #Если есть, просто отвечаем, что он уже есть
+        bot.send_message(message.from_user.id, "Вы уже есть в списке пользователей.")
+    else:
+        #Если нет, дописываю id пользователя в файл
+        users.write(user_id_str + "\n")
+        user_list.append(user_id_str)
+
+        #И отвечаю пользователю
+        bot.send_message(
+            message.from_user.id,
+            "Спасибо, добавил вас в список пользователей. "
+            "Вы получите учебный материал, как только он будет отправлен администратором."
+        )
+
+#Начинаю бесконечно слушать новые сообщения
 bot.polling(none_stop=True, interval=0)
+
+#Закрываю файл, который был открыт на запись
+users.close()
